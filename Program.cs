4202f84f2250651d88d93bdf4d1fb99af355646e
pelmen964 <ivanklimov41234@gmail.com>
2024-03-29 13:58:29 +0700

    init

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
-﻿namespace nm2
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace nm2
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            uint methodNumber,matrixRank;
+            double[,] taskMatrix;
+            double elem;
+            string[] border;
+
+            using (var sr = new StreamReader("Task.txt"))
+            {
+                methodNumber = Convert.ToUInt32(sr.ReadLine());
+                matrixRank = Convert.ToUInt32(sr.ReadLine());
+                taskMatrix = new double[matrixRank, matrixRank + 1];
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    border = sr.ReadLine().Split();
+                    for (int j = 0; j < matrixRank+1; j++)
+                    {
+                        taskMatrix[i,j] = Convert.ToDouble(border[j]);
+                    }
+                }
+            }
+            
+            //Task
+            
+            Console.Out.WriteLine("ABOBA");
+        }
     }
-  }
 }
--- Task.cs
+++ Task.cs
@@ -1,7 +1,50 @@
+using System;
+
 namespace nm2
 {
     public class Task
     {
+        /*
+        Класс должен иметь возможность
+        1. Решать СЛАУ
+        2. Поиск определителя матрицы (Гаус и декомпозиция)
+        3. Поиск обратной матрицы
+        
+        Вопрос только в том как выбирать метод решения
+        1. В файле доп. параметр
+        2. В консоле пользователем (неудобно)
+         */
+
+        private double[,] _taskMatrix;
+        private uint _matrixRank;
+
+        Task(uint matrixRank, double[,] taskMatrix)
+        {
+            _matrixRank = matrixRank;
+            _taskMatrix = new double[_matrixRank, _matrixRank + 1];
+            for (int i = 0; i < matrixRank; i++)
+            {
+                for (int j = 0; j < matrixRank+1; j++)
+                {
+                    _taskMatrix[i, j] = taskMatrix[i, j];
+                }
+            }
+        }
+
+        void Solve(uint taskType, string outFileName)
+        {
+            switch (taskType)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    throw new ArgumentException("Неверный тип задачи");
+            }
+        }
         
     }
 }
